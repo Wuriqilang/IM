5ba9e668ccaac570a57de44eb185d6b8c86428ed
Wuriqilang <543677991@qq.com>
2020-06-18 22:44:41 +0800

    11

--- im_flutter/lib/main.dart
+++ im_flutter/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
-
-void main() => runApp(new MyApp());
+void main() => runApp(
+      home: new MyApp(),
+    );
 
 class MyApp extends StatefulWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'Intelligent Manufacture System',
-
+      title: 'Intelligent Manufacture System',
+      theme: new ThemeData(primaryColor: Colors.white),
+    );
   }
 }
