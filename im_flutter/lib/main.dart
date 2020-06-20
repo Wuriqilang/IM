788e06b6abdd76b5f4d30bb52dbec388632616fe
Wuriqilang <543677991@qq.com>
2020-06-20 17:27:15 +0800

    demo

--- im_flutter/lib/main.dart
+++ im_flutter/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'model/post.dart';
+import 'demo/listview_demo.dart';
 
 void main() => runApp(App());
 
@@ -7,27 +7,13 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Home(), theme: ThemeData(primarySwatch: Colors.yellow));
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+        theme: ThemeData(primarySwatch: Colors.yellow));
   }
 }
 
 class Home extends StatelessWidget {
-  Widget _listItemBuilder(BuildContext context, int index) {
-    return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height: 16.0),
-          Text(posts[index].title, style: Theme.of(context).textTheme.title),
-          Text(posts[index].author, style: Theme.of(context).textTheme.subhead),
-          SizedBox(height: 16.0),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,24 +22,6 @@ class Home extends StatelessWidget {
           title: Text('NINGHAO'),
           elevation: 0.0,
         ),
-        body: ListView.builder(
-          itemCount: posts.length,
-          itemBuilder: _listItemBuilder,
-        ));
-  }
-}
-
-class Hello extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('hello',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 40.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.black87,
-          )),
-    );
+        body: null);
   }
 }
