d15f9714e98252d406dea683cc3feff57be9445e
Wuriqilang <543677991@qq.com>
2020-06-20 17:02:41 +0800

    priview

--- im_flutter/lib/main.dart
+++ im_flutter/lib/main.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
+import 'model/post.dart';
 
-void main() => runApp(
-      home: new MyApp(),
-    );
+void main() => runApp(App());
 
-class MyApp extends StatefulWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Intelligent Manufacture System',
-      theme: new ThemeData(primaryColor: Colors.white),
+    return MaterialApp(
+        home: Home(), theme: ThemeData(primarySwatch: Colors.yellow));
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0),
+          Text(posts[index].title, style: Theme.of(context).textTheme.title),
+          Text(posts[index].author, style: Theme.of(context).textTheme.subhead),
+          SizedBox(height: 16.0),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          title: Text('NINGHAO'),
+          elevation: 0.0,
+        ),
+        body: ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: _listItemBuilder,
+        ));
+  }
+}
+
+class Hello extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('hello',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+          )),
     );
   }
 }
