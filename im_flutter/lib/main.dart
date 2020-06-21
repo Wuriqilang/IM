d2404330fb6f0d679830e9b60e8a9abb24329c60
Wuriqilang <543677991@qq.com>
2020-06-21 20:33:52 +0800

    struct

--- im_flutter/lib/main.dart
+++ im_flutter/lib/main.dart
@@ -9,31 +9,94 @@ class App extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Home(),
-        theme: ThemeData(primarySwatch: Colors.yellow));
+        theme: ThemeData(
+            primarySwatch: Colors.yellow,
+            highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+            splashColor: Colors.white70));
   }
 }
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: "Navigration",
-            onPressed: () => debugPrint("Navigration Button is Onpressed!"),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            title: Text('NINGHAO'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: "search",
+                onPressed: () => debugPrint("seardh Button is Onpressed!"),
+              )
+            ],
+            elevation: 0.0,
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black38,
+              indicatorColor: Colors.black45,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorWeight: 1.0,
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_florist)),
+                Tab(icon: Icon(Icons.change_history)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
           ),
-          title: Text('NINGHAO'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: "search",
-              onPressed: () => debugPrint("seardh Button is Onpressed!"),
-            )
-          ],
-          elevation: 0.0,
-        ),
-        body: null);
+          body: TabBarView(
+            children: <Widget>[
+              Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+              Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+              Icon(Icons.directions_bike, size: 128.0, color: Colors.black12)
+            ],
+          ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  accountName: Text(
+                    'Wuriqilang',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  accountEmail: Text('543677991@qq.com'),
+                  currentAccountPicture: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://api.xr1228.com/static/resource/image1.jpg')),
+                  decoration: BoxDecoration(
+                    color: Colors.yellow[400],
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            'https://resources.ninghao.org/images/childhood-in-a-picture.jpg'),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.yellow[400].withOpacity(0.6),
+                            BlendMode.hardLight)),
+                  ),
+                ),
+                ListTile(
+                  title: Text('Message', textAlign: TextAlign.right),
+                  trailing:
+                      Icon(Icons.message, color: Colors.black12, size: 22.0),
+                  onTap: () => Navigator.pop(context),
+                ),
+                ListTile(
+                  title: Text('Favorite', textAlign: TextAlign.right),
+                  trailing:
+                      Icon(Icons.favorite, color: Colors.black12, size: 22.0),
+                  onTap: () => Navigator.pop(context),
+                ),
+                ListTile(
+                  title: Text('Setting', textAlign: TextAlign.right),
+                  trailing:
+                      Icon(Icons.settings, color: Colors.black12, size: 22.0),
+                  onTap: () => Navigator.pop(context),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
