b182939fb018ca7053b04309706b43464245c414
Wuriqilang <543677991@qq.com>
2020-06-24 01:25:40 +0800

    basic

---
+++ im_flutter/lib/demo/basic_demo.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class BasicDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[100],
+      child:
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+        Container(
+          child: Icon(Icons.pool, size: 32.0, color: Colors.white),
+          // color: Color.fromRGBO(3, 54, 255, 1.0),
+          padding: EdgeInsets.all(16.0),
+          margin: EdgeInsets.all(8.0),
+          width: 90.0,
+          height: 90.0,
+          decoration: BoxDecoration(
+              color: Color.fromRGBO(3, 54, 255, 1.0),
+              border: Border.all(
+                  color: Colors.indigoAccent[100],
+                  width: 3.0,
+                  style: BorderStyle.solid)),
+        )
+      ]),
+    );
+  }
+}
+
+class ReichDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+        text: TextSpan(
+            text: 'xr1228',
+            style: TextStyle(
+              color: Colors.deepPurpleAccent,
+              fontSize: 34.0,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+            ),
+            children: [
+          TextSpan(
+              text: '.com', style: TextStyle(fontSize: 17.0, color: Colors.red))
+        ]));
+  }
+}
+
+class TextDemo extends StatelessWidget {
+  final TextStyle _textStyle = TextStyle(fontSize: 16.0);
+  final String _auther = '李白';
+  final String _title = '将进酒';
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '《$_title》——$_auther “君不见，黄河之水天上来，奔流到海不复回。君不见，高堂明镜悲白发，朝如青丝暮成雪。人生得意须尽欢，莫使金樽空对月。天生我材必有用，千金散尽还复来。烹羊宰牛且为乐，会须一饮三百杯。岑夫子，丹丘生，将进酒，杯莫停。”',
+      textAlign: TextAlign.left,
+      style: _textStyle,
+      maxLines: 3,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+}
