d65644077b32ca9bf9470506c9e512b1e64fdc73
Wuriqilang <543677991@qq.com>
2020-08-01 23:51:39 +0800

    1

--- im_react/src/formDemo.jsx
+++ im_react/src/formDemo.jsx
@@ -8,12 +8,26 @@ export default class FormDemo extends React.Component {
     };
   }
 
-  handleSubmit() {}
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.value);
+  };
+
+  onChangeHandler = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.value}></input>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.onChangeHandler}
+          ></input>
+          <button type="submit">提交</button>
         </form>
       </div>
     );
