5eb955a491fa347b46c8a1c2073a356803193404
Wuriqilang <543677991@qq.com>
2020-08-05 16:49:39 +0800

    提交

---
+++ im_react/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "antd/es/button";
+import "antd/es/button/style/css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button type="dashed">Dashed Button</Button>
+      <br />
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
+    </div>
+  );
+}
+
+export default App;
--- im_react/src/index.js
+++ im_react/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import * as serviceWorker from "./serviceWorker";
-import App from "./app";
+import App from "./App";
+// import Css from "./App.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
