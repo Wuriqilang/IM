ce151ec733f197a70d51ae20ab0c1ad37620120e
Wuriqilang <543677991@qq.com>
2020-08-16 20:48:16 +0800

    study

--- im_react/src/App.jsx
+++ im_react/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import AntdDemo from "./components/AntdDemo";
+import Home from "./pages/Home";
+import Mine from "./pages/Mine";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <AntdDemo />
+        {/* <AntdDemo /> */}
+        <Router>
+          <Route path="/home" component={Home}></Route>
+          <Route path="/mine" component={Mine}></Route>
+        </Router>
       </div>
     );
   }
